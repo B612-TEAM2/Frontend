413ef5afad5dbc5b5dbe0a18ffd8b1c813d14c1f
nijyoumis <yuooo26@naver.com>
2024-02-13 17:16:01 +0900

    editing friend components

--- src/atom.js
+++ src/atom.js
@@ -14,3 +14,9 @@ export const isHomeMap = atom({
   key: "isHomeMap",
   default: false,
 });
+
+//back으로 부터 받아온 친구 글의 위도, 경도, pid 정보
+export const friendMarkers = atom({
+  key: "friendMarkers",
+  default: null,
+});
