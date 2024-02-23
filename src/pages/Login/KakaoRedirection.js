a404ebaa2cf45e3f647642ce9cbe00a291c29d3a
nijyoumis <yuooo26@naver.com>
2024-02-23 19:07:37 +0900

    kakao, google 수정

--- src/pages/Login/KakaoRedirection.js
+++ src/pages/Login/KakaoRedirection.js
@@ -3,34 +3,25 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function KakaoRedirection() {
-  const code = new URL(document.location.toString()).searchParams.get('code');
+  const code = new URL(document.location.toString()).searchParams.get("code");
   const navigate = useNavigate();
 
   useEffect(() => {
     async function KakaoLogin() {
-      const res = await axios.post(
-        process.env.REACT_APP_KAKAO_URL
-        , {
-          code: code
-        }
-      );
+      const res = await axios.post(process.env.REACT_APP_KAKAO_URL, {
+        code: code,
+      });
       const ACCESS_TOKEN = res.data["access-token"];
       const REFRESH_TOKEN = res.data["refresh-token"];
-      
+
       localStorage.setItem("accessToken", ACCESS_TOKEN);
       localStorage.setItem("refreshToken", REFRESH_TOKEN);
-    };
-
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      navigate("/home", {replace: true});
-    } else {
-      KakaoLogin();
-      navigate("/SetProfile", {replace: true})
     }
+    KakaoLogin();
+    navigate("/SetProfile", { replace: true });
   }, []);
 
   return <div>로그인 중입니다...</div>;
-};
+}
 
 export default KakaoRedirection;
