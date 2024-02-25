a85fa1aade8bfe5c4e820f16b4cb84f03d0b4b5a
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-25 13:09:37 +0900

    fix postInfo url

--- src/components/Home/PostPage.js
+++ src/components/Home/PostPage.js
@@ -13,7 +13,7 @@ const PostPage = () => {
     const fetchPost = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:8080/postInfo/`,
+          `http://localhost:8080/postInfo`,
           {
             params: { pid: id },
             headers: {
