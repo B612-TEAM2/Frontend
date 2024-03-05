294e77b3ebba9711830d6ea776c84c7a916f53c7
은정 <eunjung@eunjeongs-MacBook-Pro.local>
2024-03-05 19:53:08 +0900

    token

--- src/components/Token/TokenRefresher.js
+++ src/components/Token/TokenRefresher.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 
-export default function TokenRefresher() {
+export default function TokenRefresher({ children }) {
     const navigate = useNavigate();
+    const [isTokenRefreshed, setIsTokenRefreshed] = useState(false);
 
     useEffect(() => {
         const checkTokenExpiration = async () => {
@@ -18,25 +19,30 @@ export default function TokenRefresher() {
                     const refreshToken = localStorage.getItem('refreshToken');
                     if(refreshToken) {
                         try {
-                            const response = await axios.post('/api/jwt/access', {
+                            const response = await axios.post('/api/jwt/access', {}, {
                                 headers: {
                                     Authorization: `Bearer ${refreshToken}`,
                                   },
                         });
                         console.log('백엔드에 request 전송:', response);
                         localStorage.setItem('accessToken', response.data["access-token"]);
+                        setIsTokenRefreshed(true);
                         console.log('New access token:', response.data.access);
                         } catch(error) {
                             alert('로그인 유효 기간이 만료되었습니다. 다시 로그인해 주세요.');
                             navigate('/');
                         }
                 }
+            } else {
+                setIsTokenRefreshed(true);
             }
+        } else {
+            setIsTokenRefreshed(true);
         }
         };
 
     checkTokenExpiration();
     }, [navigate]);
 
-return null;
+return isTokenRefreshed ? children : null;
 }
